5e4b5a451b7a5a2f6bcd7f9095f710fe8e14cead
CheerfulChen <Cheerful_Chen5@163.com>
2026-02-16 23:11:06 +0800

    Initial Commit

---
+++ TimeCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeCalculator
+//
+//  Created by 颜宇辰 on 2026/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeCalculator/TimeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCalculatorApp.swift
+//  TimeCalculator
+//
+//  Created by 颜宇辰 on 2026/2/16.
+//
+
+import SwiftUI
+
+@main
+struct TimeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
